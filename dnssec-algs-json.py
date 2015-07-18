b2a02be8747ad778c2dfc2a0e32fe349eced40d4
Dan York <dyork@lodestar2.com>
2015-07-18 17:18:12 +0200

    Updated main program to retrieve CSV from the IANA site.

--- dnssec-algs-json.py
+++ dnssec-algs-json.py
@@ -1,8 +1,27 @@
+# A short program to read the IANA registry of DNSSEC algorithm numbers found at
+# http://www.iana.org/assignments/dns-sec-alg-numbers/dns-sec-alg-numbers-1.csv
+# and print it out as a JSON object.
+#
+# Note that the JSON file has two primary differences from the IANA registry:
+# 1. Only 3 of the 6 fields are included: 'Number','Mnemonic' and 'Description'.
+# 2. Rows are removed if 'Description' is set to 'Reserved' or 'Unassigned'.
+#
+# Created by Dan York - dyork@lodestar2.com
+# Comments, feedback and pull requests are welcome.
+# Repository: https://github.com/danyork/dnssec-algs-json
+#
+
 import csv
 import json
+import urllib2
 
-f = open('dns-sec-alg-numbers-1.csv','r')
-output = open('dns-sec-alg-numbers.json','w')
+#f = open('dns-sec-alg-numbers-1.csv','r')
+
+try:
+    f = urllib2.urlopen('http://www.iana.org/assignments/dns-sec-alg-numbers/dns-sec-alg-numbers-1.csv')
+except: 
+    print("Unable to connect to IANA server.")
+    exit()
 
 algs = csv.DictReader(f)
 print "["
@@ -13,8 +32,3 @@ for row in algs:
     
 print "]"
     
-#    print json.dumps([row['Number'],row['Mnemonic'],row['Description']])
-    
-#    print (row['Number'] + ' ' + row['Mnemonic'] + ' ' + row['Description'])
-
-#print json.dump(algs,output)
